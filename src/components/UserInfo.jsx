06258c1eaedffc5a90d8d59d3600f8e52167942c
Roberto <109484020+robrobac@users.noreply.github.com>
2023-05-19 18:21:05 +0200

    User info styling

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,21 +2,32 @@ import React from 'react'
 import './UserInfo.scss';
 
 function UserInfo({user}) {
-    
-  return (
-    <div className='userInfo'>
-        <img className='userImage'
-            src={user.avatar_url}
-            alt="user_avatar"
-            style={{display: user.id ? 'block' : 'none' }}>
-        </img>
-        <h2 className='username'>{user.name}</h2>
-        <h3 className='login'>{user.login}</h3>
-        <p className='bio'>{user.bio}</p>
-        <p className='location'>{user.location}</p>
-        
-    </div>
-  )
+    if(user.login) {
+        return (
+            <div className='userInfo'>
+                <img className='userImage'
+                    src={user.avatar_url}
+                    alt="user_avatar"
+                    style={{display: user.id ? 'block' : 'none' }}>
+                </img>
+                <h2 className='username'>{user.name}</h2>
+                <h3 className='login'>{user.login}</h3>
+                <p className='bio'>{user.bio}</p>
+                <a href={`https://github.com/${user.login}`} target='blank' className='profileLink'>
+                    <p>View Profile</p>
+                </a>
+                <div className='follow'>
+                    <p>
+                        <span>{user.followers} </span>
+                        {user.followers === 1 ? "follower" : "followers"}
+                    </p>
+                    -
+                    <p><span>{user.following} </span>following</p>
+                </div>
+                <p className='location'>{user.location}</p>
+            </div>
+          )
+    }
 }
 
 export default UserInfo
